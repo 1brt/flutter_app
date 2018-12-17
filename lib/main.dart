96f7bddb7e78869c1f414db6b610437d46ade063
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2018-12-17 18:21:17 -0200

    Tratado stateful widget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Meu Primeiro App'),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/food.jpg'),
-              Text('Food Paradise')
-            ],
+          appBar: AppBar(
+            title: Text('Meu Primeiro App'),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10),
+                child: RaisedButton(
+                  child: Text('Add Product'),
+                  onPressed: () {},
+                ),
+              ),
+              Card(
+                child: Column(
+                  children: [
+                    Image.asset('assets/food.jpg'),
+                    Text('Food Paradise')
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
