808f0023c9376404547173877b9dc29ca47d172c
1brt <hvcarvalhobrt@gmail.com>
2018-12-17 15:27:38 -0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Meu Primeiro App v1.0'),
+          title: Text('Meu Primeiro App'),
         ),
         body: Card(
           child: Column(
